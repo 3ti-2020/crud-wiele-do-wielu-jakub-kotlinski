c5927bd69b5beb85c5e35d016389edbff8358080
Kotlet420 <49307041+Kotlet420@users.noreply.github.com>
2020-11-03 11:16:03 +0100

    Dodawanie do tabeli

--- insert_autorzy.php
+++ insert_autorzy.php
@@ -1,19 +1,39 @@
 <?php 
 
-//$conn = new mysqli("localhost","root","","zadania");
-$conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
-$imie = $_POST["name"];
-$tytul = $_POST["tytul"];
+$conn = new mysqli("localhost","root","","zadania");
+//$conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
+//$imie = $_POST["name"];
+//$tytul = $_POST["tytul"];
 
-$conn->query("INSERT INTO `lib_autor`(`id`, `name`) VALUES (NULL,'".$imie."');");
+/*$conn->query("INSERT INTO `lib_autor`(`id`, `name`) VALUES (NULL,'".$imie."');");
 
-$conn->query("INSERT INTO `lib_autor`(`id`, `tytul`) VALUES (NULL,'".$tytul."');");
+$conn->query("INSERT INTO `lib_tytul`(`id_tytul`, `tytul`) VALUES (NULL,'".$tytul."');");
 
 
-//header('Location: /php/cos/index.php');
-header('Location: https://jakub-kotlinski.herokuapp.com/');
+header('Location: /php/cos/index.php');
+//header('Location: https://jakub-kotlinski.herokuapp.com/');*/
+
+$sqlGetId = "SELECT LAST_INSERT_ID()";
+
+$sql=" INSERT INTO lib_autor (id, name) values (NULL, '".$_POST['name']."') ";
+mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sqlGetId);
+while ($row = $result->fetch_assoc()) {
+    $autorId = $row["LAST_INSERT_ID()"];
+}
+
+$sql=" INSERT INTO lib_tytul (id_tytul, tytul) values (NULL, '".$_POST['tytul']."') ";
+mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sqlGetId);
+while ($row = $result->fetch_assoc()) {
+    $tytulId = $row["LAST_INSERT_ID()"];
+}
+
+$sql=" INSERT INTO lib_autor_tytul (id_autor_tytul, id_autor,id_tytul) values (NULL, $autorId, $tytulId)";
+
+mysqli_query($conn, $sql);
 
 $conn->close(); 
-
+header('Location: /php/cos/index.php');
 
 ?>
