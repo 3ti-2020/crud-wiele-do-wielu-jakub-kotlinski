5fda14aecb32cf96d2ccae80c1da186f23627954
Kotlet420 <49307041+Kotlet420@users.noreply.github.com>
2020-11-10 15:48:59 +0100

    Add files via upload

---
+++ logowanie.php
@@ -0,0 +1,13 @@
+<?php
+
+    session_start();
+    //$conn = new mysqli("localhost","root","","zadania");
+    $conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
+
+    if ($conn->query("SELECT * FROM `users` WHERE `username` = '".$_POST['login']."' AND `userpass` = '".$_POST['password']."'")->fetch_assoc()) $_SESSION['login'] = $_POST['login'];
+    sleep(4);
+
+    //header('Location: /admin/index2.php');
+    header('Location: https://jakub-kotlinski.herokuapp.com/admin/index2.php');
+
+?>
