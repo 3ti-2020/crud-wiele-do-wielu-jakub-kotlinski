cb5f83707b28284797a53de6f19afb55557dd9a7
Kotlet420 <49307041+Kotlet420@users.noreply.github.com>
2020-11-10 14:57:10 +0100

    teraz to już na pewno funkcje dla administratora

--- delet.php
+++ delet.php
@@ -1,12 +1,12 @@
 <?php
-    $conn = new mysqli("localhost","root","","zadania");
-    //$conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
+    //$conn = new mysqli("localhost","root","","zadania");
+    $conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
 
     $del = $_POST['id_del'];
 
     $sql = "DELETE FROM lib_autor_tytul WHERE id_autor_tytul=$del";
     mysqli_query($conn, $sql);
 
-    header('Location: /php/cos/index2.php');
-    //header('Location: https://jakub-kotlinski.herokuapp.com/');
+    //header('Location: /php/cos/index2.php');
+    header('Location: https://jakub-kotlinski.herokuapp.com/index2.php');
 ?>
--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
 
     session_start();
-    $conn = new mysqli("localhost","root","","zadania");
-    //$conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
+    //$conn = new mysqli("localhost","root","","zadania");
+    $conn = new mysqli("sql7.freemysqlhosting.net","sql7373143","ky7DfUhHKN","sql7373143");
 
     unset($_SESSION['login']);
 
-    header('Location: /php/cos/index.php');
-    //header('Location: https://jakub-kotlinski.herokuapp.com/');
+    //header('Location: /php/cos/index.php');
+    header('Location: https://jakub-kotlinski.herokuapp.com/');
 
 ?>
