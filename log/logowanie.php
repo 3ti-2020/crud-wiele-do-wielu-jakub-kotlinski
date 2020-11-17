980814fe5aa0b4bc3650a7b473ffc56addde4a0a
Kotlet420 <49307041+Kotlet420@users.noreply.github.com>
2020-11-17 15:33:06 +0100

    Zmiana na lepsze

--- log/logowanie.php
+++ log/logowanie.php
@@ -4,8 +4,17 @@
     //$conn = new mysqli("localhost","root","","zadania");
     $conn = new mysqli("remotemysql.com","jfFxpXyGWk","Dly0LzRmEd","jfFxpXyGWk");
 
-    if ($conn->query("SELECT * FROM `users` WHERE `username` = '".$_POST['login']."' AND `userpass` = '".$_POST['password']."'")->fetch_assoc()) $_SESSION['login'] = $_POST['login'];
+    if ($conn->query("SELECT * FROM `users` WHERE `username` = '".$_POST['login']."' AND `userpass` = '".$_POST['password']."'")->fetch_assoc())
+    {
+        $_SESSION['login'] = $_POST['login'];
+        header('Location: /admin/index2.php');
+    }
+    else
+    {
+        header('Location: /index.php');
+    }
+    
 
-    header('Location: /admin/index2.php');
+    
 
 ?>
